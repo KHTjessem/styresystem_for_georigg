b264852bce1b7a064f8b9ed16de3a12fb6e3986b
Kristian H. Tjessem <kristian.tjessem@outlook.com>
2021-02-04 16:22:36 +0100

    Get engines sensor measured position correclty.

--- source/engineControll/controll.py
+++ source/engineControll/controll.py
@@ -7,24 +7,36 @@ class controll:
         self.__connection = comSerial.connection('COM3')
 
     
-    # For now harcoded numbers, needs to be changed.
-    # Supposed to be the command:
-    #   Rotate left motor 0, velocity 10.
-    #   Mnemonic: ROL 0, 10.
-    def rotate_left(self):
-        ROL = command(1, 2, 0, 0, -100)
-        self.__connection.write(ROL.getByteArray())
+    # adress and choice of motor/axis is hardcoded.
+    # TODO: get module adress, engine/axis etc from "dynamic" source
+    def rotate_right(self, velocity):
+        ROR = command(1,1, 0, 0, velocity)
+        return self.__connection.write(ROR.getByteArray())
+
+    def rotate_left(self, velocity):
+        ROL = command(1, 2, 0, 0, velocity)
+        s = self.__connection.write(ROL.getByteArray())
+        print(s)
     
     def stop(self):
         MST = command(1, 3, 0, 0, 0)
-        self.__connection.write(MST.getByteArray())
+        s = self.__connection.write(MST.getByteArray())
+        print(s)
     
+
+    def getActualPosition(self):
+        GAP = command(1, 6, 1, 0, 0)
+        s = self.__connection.write(GAP.getByteArray())
+        print("Position: " + str(int.from_bytes(s.value, 'big', signed=True)))
+        print(str(s.value))
+
     def close(self):
         self.__connection.close()
 
 
 if __name__ == "__main__":
     c = controll()
-    c.rotate_left()
+    c.rotate_left(50)
     c.stop()
+    c.getActualPosition()
     c.close()
