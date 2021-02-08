92e287303f417d7d0db2c4ef164bac36f2ca9fa1
Kristian H. Tjessem <kristian.tjessem@outlook.com>
2021-02-08 13:10:17 +0100

    Set up for testing functions ror, rol, and stop.

--- source/main.py
+++ source/main.py
@@ -3,7 +3,24 @@ import sys
 
 import random
 
+from engineControll.controll import controll
+
+# Engine init
+cont = controll()
+
+# EEL init
 eel.init('web')
+
+@eel.expose
+def rotate_right(velocity):
+    cont.rotate_right(velocity)
+@eel.expose
+def rotate_left(velocity):
+    cont.rotate_left(velocity)
+@eel.expose
+def stop():
+    cont.stop()
+
 
 
 @eel.expose
@@ -11,4 +28,11 @@ def rand_numb():
     return random.randint(0, 100)
 
 
-eel.start('index.html')
+def close_callback(route, websockets):
+    if not websockets:
+        cont.stop()
+        cont.close()
+        exit()
+
+eel.start('index.html', close_callback=close_callback)
+
